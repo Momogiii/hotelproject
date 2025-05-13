035230258cb4e557fbf999d6c76e0adf9dc18463
Momogiii <mmomogi17@gmail.com>
2025-05-13 07:00:23 +0700

    Upload project

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\KamarController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\TamuController;
 
 
 Route::get('/', function () {
@@ -15,6 +16,7 @@ Route::get('backend/login', [LoginController::class, 'loginform']);
 Route::get('backend/daftar', [LoginController::class, 'daftarform']);
 Route::get('backend/kamar', [KamarController::class, 'kamar']);
 Route::get('backend/booking', [BookingController::class, 'booking']);
+Route::get('backend/tamu', [TamuController::class, 'tamu']);
 Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda');
 Route::post('backend/login', [LoginController::class, 'simpanBeranda'])->name('backend.login');
 Route::post('backend/daftar', [LoginController::class, 'register'])->name('backend.register');
